a38629eb2bc7e218ec8d8e0df18640ae865ec0bc
Elivelton <eliveltonmn@outlook.com>
2022-06-23 18:25:26 -0300

    Feito 1 exercicio do dia 3 bloco 4 de fundamentos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-da-programacao-e-algoritmos/script-e1.js
@@ -0,0 +1,12 @@
+let fatorial = 4;
+// let fatorial = 10;
+let soma = fatorial;
+
+// for (let index = 9; index > 1; index -=1  ){
+for (let index = 3; index > 1; index -=1  ){
+
+    soma *= index;
+
+}
+console.log("---");
+console.log(soma);
