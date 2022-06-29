a4cebfb2a5911445b4109e088a3e4533783c9be4
Elivelton <eliveltonmn@outlook.com>
2022-06-29 18:23:37 -0300

    Exercicio 2 - ok

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio-aula/exercicio 1/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio-aula/exercicio 1/script.js
@@ -7,3 +7,13 @@ let tagH1 = document.createElement('h1');
 tagH1.textContent = 'Exercício 5.2 - JavaScript DOM';
 
 body.appendChild(tagH1);
+
+//Exercicio 2
+
+let tagMain = document.createElement('main');
+
+tagMain.classList.add('main-content');
+
+body.appendChild(tagMain);
+
+//Exercicio 3
