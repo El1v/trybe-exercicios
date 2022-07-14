23fc3c545da591bcbf1a4b2bdc00f8e2b43acf61
Elivelton <eliveltonmn@outlook.com>
2022-07-14 18:30:03 -0300

    exercicio 2

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -1,13 +1,26 @@
-const testingScope = (escopo) => {
+//exercicio 1
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//     ifScope = ifScope + ` ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+//     console.log(elseScope);
+//   }
+// }
 
-  if (escopo === true) {
-    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-    ifScope = ifScope + ` ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-    console.log(elseScope);
+// testingScope(true);
+
+//exercicio 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a,b) => {
+  if(a > b) return 1;
+  if(a < b) return -1;
+  return 0;  
   }
-}
+)
 
-testingScope(true);
+console.log(`Os numeros ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
