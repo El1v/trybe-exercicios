61a8146922a0e5f27aa1eee46f4c0f071d045b87
Elivelton <eliveltonmn@outlook.com>
2022-07-15 15:42:04 -0300

    Exercicios da aula assincrona

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecoes-e-objetos/script.js
@@ -0,0 +1,41 @@
+// const student1 = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkills: 'Ótimo',
+// };
+
+// const student2 = {
+//   Html: 'Bom',
+//   Css: 'Ótimo',
+//   JavaScript: 'Ruim',
+//   SoftSkills: 'Ótimo',
+//   Git: 'Bom', // chave adicionada
+// };
+
+// const listSkills = (student) => {
+//   const arraySkills = Object.keys(student);
+//   for (const index in arraySkills) {
+//     console.log(`${arraySkills[index]}, Nivel: ${student[arraySkills[index]]}`);
+//   }
+// }
+
+
+// listSkills(student1);
+
+//---------------------
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
