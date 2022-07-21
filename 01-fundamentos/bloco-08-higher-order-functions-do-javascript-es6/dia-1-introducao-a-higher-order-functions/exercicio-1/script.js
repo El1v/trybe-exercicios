d49a8d4bfcf3ecf49790c591d415169900d92750
Elivelton <eliveltonmn@outlook.com>
2022-07-21 16:06:46 -0300

    Exercicio 1 do primeiro dia do bloco 8

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercicio-1/script.js
@@ -0,0 +1,20 @@
+const person = (name) => {
+  const emailLayout = name.toLowerCase().split(' ').join('_');
+  const email = `${emailLayout}@trybe.com`
+  return {
+    nome: name,
+    email: email,
+  }
+}
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva'),
+  }
+  return employees;
+}
+
+console.log(newEmployees(person));
+
